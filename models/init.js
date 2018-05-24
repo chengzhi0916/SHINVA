49b77be32027a18df29c3ad22cb1b033729b56ee
chengzhi0916 <523183756@qq.com>
2018-05-24 22:42:48 +0800

    views/create.ejs

---
+++ models/init.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://118.89.183.32:27017/config');
---
+++ route.page.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var PostModel = require('./models/post');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET posts page. */
+router.get('/posts', function(req, res, next) {
+  res.render('posts', { title: 'posts'} );
+});
+
+/* GET posts edit page. */
+router.get('/posts/create', function(req, res, next) {
+  res.render('create');
+});
+
+router.get('/posts/show', function (req, res, next) {
+  var id = req.query.id;
+
+  PostModel.findOne({_id: id}, function (err, post) {
+    post.content = marked(post.content);
+    res.render('show', {post});
+  });
+});
+
+/* GET posts edit page. */
+router.get('/posts/edit', function (req, res, next) {
+  var id = req.query.id;
+
+  res.render('edit', { id });
+});
+
+module.exports = router;
